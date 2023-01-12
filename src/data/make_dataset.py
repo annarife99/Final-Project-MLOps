729479750525c2ed0219295f874d6cbafea6d5cb
annarife99 <annarife99@gmail.com>
2023-01-12 14:39:57 +0100

    dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,19 +4,48 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 import os
-import hydra
-
+import pandas as pd
+import torch 
+import csv
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+#@click.argument('input_filepath', type=click.Path(exists=True))
+#@click.argument('output_filepath', type=click.Path())
+
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('Downloading dataset from Kaggle')
+    logger.info('Making final data set from raw data')
+
+    path=os.getcwd()
+    pd_file_train = pd.read_csv(path+'/data/raw/Corona_NLP_train.csv',chunksize=50000)
+    full_data= pd.concat(pd_file_train)
+    print(full_data)
+    #44955+1
+    pd_file_test = pd.read_csv(path+'/data/raw/Corona_NLP_test.csv')
+    #CHANGE LABELS
+    pd_file_test.loc[pd_file_test['Sentiment'] == "Extremely Negative", 'Sentiment'] = 0
+    pd_file_test.loc[pd_file_test['Sentiment'] == "Negative", 'Sentiment'] = 1
+    pd_file_test.loc[pd_file_test['Sentiment'] == "Neutral", 'Sentiment'] = 2
+    pd_file_test.loc[pd_file_test['Sentiment'] == "Positive", 'Sentiment'] = 3
+    pd_file_test.loc[pd_file_test['Sentiment'] == "Extremely Positive", 'Sentiment'] = 4
+
+
+    train_dic={}
+    train_dic['labels']= pd_file_train['Sentiment'].to_numpy()
+    train_dic['tweets']= pd_file_train['OriginalTweet'].to_numpy()
+
+    test_dic={}
+    test_dic['labels']= pd_file_test['Sentiment'].to_numpy()
+    test_dic['tweets']= pd_file_test['OriginalTweet'].to_numpy()
+
+    print(train_dic['labels'])
+
+    
+    #torch.save(test_dic, path+'/data/processed/test.pth')
 
 
 if __name__ == '__main__':
