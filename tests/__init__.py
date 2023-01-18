e0f58d6f6a3a6985611ab4a0abf0a0c88f83359e
lluisDTU <lluiscolomercoll@gmail.com>
2023-01-18 12:17:42 +0100

    Implement unittesting

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,4 @@
 import os
-from tests.test_data import test_data
 _TEST_ROOT = os.path.dirname(__file__)  # root of test folder
 _PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
-_PATH_DATA = os.path.join(_PROJECT_ROOT, "Data")  # root of data
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
---
+++ tests/test_training.py
@@ -0,0 +1,14 @@
+"""implement at least one test that asserts something about your training script.
+You are here given free hands on what should be tested, but try to test something
+he risk being broken when developing the code."""
+
+"""from src.models.train_model import main
+
+def test_accuracy(self):
+    # Train the model
+    accuracy = main()
+    # Evaluate the model
+    # Assert that the accuracy is above a certain threshold
+    self.assertGreaterEqual(accuracy, 0.20)
+"""
+
